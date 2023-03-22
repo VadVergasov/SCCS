fcf613438bd57939ede9d5ec1cddf169a5e5dcfc
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-03-22 23:58:02 +0300

    Added entrypoint

--- 4/2/1/main.py
+++ 4/2/1/main.py
@@ -11,14 +11,20 @@ from utilities.helpers import (
     get_top_k_n_grams,
 )
 
-try:
-    with open(os.path.join("data", "text.txt"), "r", encoding="utf8") as fp:
-        text = fp.read()
-except FileNotFoundError:
-    print("No text.txt file, nothing to analyze!")
-    sys.exit(1)
 
-print(get_non_declarative_sentences_count(text))
-print(get_average_words_count(text))
-print(get_average_word_length(text))
-print(get_top_k_n_grams(text))
+def main() -> None:
+    try:
+        with open(os.path.join("data", "text.txt"), "r", encoding="utf8") as fp:
+            text = fp.read()
+    except FileNotFoundError:
+        print("No text.txt file, nothing to analyze!")
+        sys.exit(1)
+
+    print(get_non_declarative_sentences_count(text))
+    print(get_average_words_count(text))
+    print(get_average_word_length(text))
+    print(get_top_k_n_grams(text))
+
+
+if __name__ == "__main__":
+    main()
