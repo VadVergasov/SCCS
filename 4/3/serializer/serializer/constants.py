da2e4d4d4fa012f5e2fa882d02be9fd45a521161
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-05-03 19:42:44 +0300

    Added serializer for primitive

---
+++ 4/3/serializer/serializer/constants.py
@@ -0,0 +1,5 @@
+TYPE = r"\'(\w+)\'"
+
+OBJECT_ANNOTATION = "object"
+TYPE_ANNOTATION = "type"
+VALUE_ANNOTATION = "value"
--- 4/3/serializer/serializer/serializer.py
+++ 4/3/serializer/serializer/serializer.py
@@ -2,16 +2,42 @@
 Implementing serializer class
 """
 import inspect
+from typing import Callable
+
+import re
+from .constants import TYPE, TYPE_ANNOTATION, VALUE_ANNOTATION, OBJECT_ANNOTATION
+
 
 class Serializer:
     """
     Serializer class
     """
-    def serialize(self, obj: object):
+
+    @staticmethod
+    def serialize(obj: object):
         """
         Serialization function
         """
-        if inspect.isclass(obj):
-            pass
-        else:
-            pass
+        serializer: Callable[[object], dict[str, object]] = Serializer.__get_serializer(obj)
+        result = tuple((key, value) for key, value in serializer(obj).items())
+
+        return result
+
+    @staticmethod
+    def __get_serializer(obj: object) -> Callable[[object], dict[str, object]]:
+        """
+        Gets serializer for object
+        """
+        if isinstance(obj, (float, int, complex, bool, str, type(None))):
+            return Serializer.__serialize_primitive
+
+    @staticmethod
+    def __serialize_primitive(obj: object) -> dict[str, object]:
+        """
+        Serializes primitive type
+        """
+        serialized_primitive: dict[str, object] = {}
+        serialized_primitive[TYPE_ANNOTATION] = re.search(TYPE, str(type(obj))).group(1)
+        serialized_primitive[VALUE_ANNOTATION] = obj
+
+        return serialized_primitive
