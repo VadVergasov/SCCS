2f1e73477905086e6df48ab6993f71c7241971bc
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-05-28 21:27:28 +0300

    Added json serializer

--- 4/3/serializer/factory/parsers/json/parser.py
+++ 4/3/serializer/factory/parsers/json/parser.py
@@ -5,37 +5,81 @@ import io
 
 from serializer.factory.parsers.parser import Parser
 
+from serializer.serializer.serializer import Serializer
+
 
 class ParserJson(Parser):
     """
     JSON parser
     """
 
-    def dump(self, obj: object, fp: io.IOBase) -> None:
+    @staticmethod
+    def __serialize_json(obj: object) -> str:
+        """
+        Serializes to json format
+        """
+        if type(obj) == tuple:
+            serialized = []
+            for current in obj:
+                serialized.append(f"{ParserJson.__serialize_json(current)}")
+            return f"[{', '.join(serialized)}]"
+        else:
+            return f'"{str(obj)}"'
+
+    @staticmethod
+    def __deserialize_json(obj: str) -> object:
+        if obj == "[]":
+            return tuple()
+        if obj[0] == "[":
+            obj = obj[1 : len(obj) - 1]
+            deserialized_obj = []
+            depth = 0
+            is_quote = False
+            substr = ""
+            for i in obj:
+                if i == "[":
+                    depth += 1
+                elif i == "]":
+                    depth -= 1
+                elif i == '"':
+                    is_quote = not is_quote
+                elif i == "," and not is_quote and depth == 0:
+                    deserialized_obj.append(ParserJson.__deserialize_json(substr))
+                    substr = ""
+                    continue
+                elif i == " " and not is_quote:
+                    continue
+                substr += i
+            deserialized_obj.append(ParserJson.__deserialize_json(substr))
+            return tuple(deserialized_obj)
+        return obj[1 : len(obj) - 1]
+
+    @staticmethod
+    def dump(obj: object, fp: io.IOBase) -> None:
         """
         Prints object to fp
         """
-        fp.write(self.dumps(obj))
+        fp.write(ParserJson.dumps(obj))
 
     @staticmethod
     def dumps(obj: object) -> str:
         """
         Returns string with serialized obj
         """
-        # TODO
-        pass
+        serialized = Serializer.serialize(obj)
+        return ParserJson.__serialize_json(serialized).replace("\n", "\\n")
 
     @staticmethod
     def load(fp: io.IOBase) -> object:
         """
         Returns object serialized from obj
         """
-        return self.loads(fp.read())
+        return ParserJson.loads(fp.read())
 
     @staticmethod
     def loads(string: str) -> object:
         """
         Returns object serialized from obj
         """
-        # TODO
-        pass
+        obj = ParserJson.__deserialize_json(string)
+        return Serializer.deserialize(obj)
