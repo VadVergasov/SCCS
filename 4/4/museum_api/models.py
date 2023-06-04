8aaf106344e75ae3245e11da7a54a3666473de23
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-06-04 16:27:17 +0300

    Added fields to models

--- 4/4/museum_api/models.py
+++ 4/4/museum_api/models.py
@@ -20,6 +20,17 @@ class Exhibit(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     art_type = models.ForeignKey(ArtType, on_delete=models.CASCADE)
+    date_arrived = models.DateField(auto_now_add=True)
+    responsible = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def delete(self, using=None, keep_parents=False):
+        responsible_user = User.objects.filter(groups__name="Сотрудники").first()
+
+        if responsible_user:
+            self.responsible = responsible_user
+            self.save()
+
+        super().delete(using=using, keep_parents=keep_parents)
 
     def __str__(self):
         return str(self.name)
@@ -28,6 +39,9 @@ class Exhibit(models.Model):
 class Gallery(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    number = models.IntegerField()
+    floor = models.IntegerField(null=True)
+    area = models.FloatField(null=True)
 
     def __str__(self):
         return str(self.name)
