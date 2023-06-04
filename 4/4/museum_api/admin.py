f0d0b82faf72203be16e4f508e01dccabe87c13c
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-06-04 18:02:18 +0300

    Added tours to admin panel

--- 4/4/museum_api/admin.py
+++ 4/4/museum_api/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
-from museum_api.models import ArtType, Exhibit, Gallery, Position, User
+from museum_api.models import ArtType, Exhibit, Gallery, Position, User, Tour
 
 admin.site.register(ArtType)
 admin.site.register(Exhibit)
 admin.site.register(Gallery)
 admin.site.register(Position)
+admin.site.register(Tour)
 
 
 class UserAdmin(DefaultUserAdmin):
